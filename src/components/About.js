26d934be4c4ad1f7170afeec7e0a8d51b8f48607
Eric <mugoericpg20@gmail.com>
2023-04-22 11:53:56 +0300

    Exported About

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+function About(props){
+const image = props.image || "https://via.placeholder.com/215"
+
+    return (
+        <aside>
+            <img src = {image} alt = "blog logo"></img>
+            <p>{props.aboutText}</p>
+        </aside>
+    )
+}
